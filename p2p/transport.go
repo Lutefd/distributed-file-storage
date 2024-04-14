0c2069a39d5c4ca8f1fd741ca7576bd91c1ba993
Luis Dourado <lfaddourado@gmail.com>
2024-04-14 19:08:14 -0300

    feat(transport): scaffold of transport and peer generics

---
+++ p2p/transport.go
@@ -0,0 +1,15 @@
+package p2p
+
+// Peer is a generic interface for a representation of a remote node, which is anything that can send and receive data
+type Peer interface {
+	Send(data []byte) error
+	Receive() ([]byte, error)
+
+}
+
+// Transport is a generic interface for a transport layer, which is anything that handles the communication between nodes in the network
+// This can be iof the form of TCP, UDP, websockets, etc.
+type Transport interface {
+	ListenAndAccept() error
+	
+}
